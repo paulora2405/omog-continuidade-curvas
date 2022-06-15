106cad89688c4e5878862c7bf818a4b9ffb5e201
Paulo Albuquerque <paulora2405@gmail.com>
2022-06-15 20:35:41 -0300

    Add control point create and move

--- controlpoint.py
+++ controlpoint.py
@@ -1,23 +1,38 @@
 import pygame
+from colors import *
 
 pygame.init()
-font = pygame.font.Font('freesansbold.ttf', 24)
+font = pygame.font.Font('freesansbold.ttf', 16)
 
 
 class ControlPoint:
-    def __init__(self, x, y, text=" "):
+    def __init__(self, x: int, y: int, id: int):
         self.x = x
         self.y = y
-        self.text = text
+        self.id = id
+        self.inner_circle = None
+        self.outter_circle = None
 
-    def display(self, screen, color):
-        text = font.render(self.text, True, color)
+    def get_pos(self) -> tuple[int, int]:
+        return (self.x, self.y)
+
+    def set_pos(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def is_bellow(self, x: int, y: int):
+        return self.outter_circle.collidepoint(x, y)
+
+    def draw(self, screen: pygame.Surface, color):
+        text = font.render(f'P{self.id}', True, color)
         textRect = text.get_rect()
-        if self.y > 540:
-            textRect.center = (self.x, self.y + 30)
+        if self.y < 20:
+            textRect.center = (self.x, self.y + 20)
         else:
-            textRect.center = (self.x, self.y - 35)
+            textRect.center = (self.x, self.y - 15)
 
         screen.blit(text, textRect)
-        pygame.draw.circle(screen, color, (self.x, self.y), 12)
-        pygame.draw.circle(screen, (120, 120, 120), (self.x, self.y), 10)
+        self.outter_circle = pygame.draw.circle(
+            screen, color, (self.x, self.y), 10)
+        self.inner_circle = pygame.draw.circle(
+            screen, grey, (self.x, self.y), 9)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 from colors import *
+from controlpoint import ControlPoint
 
-size = (1600, 900)
+size = (1280, 720)
 
 pygame.init()
 screen = pygame.display.set_mode(size)
+print(screen.get_size())
 clock = pygame.time.Clock()
 fps = 60
-
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font('freesansbold.ttf', 26)
+control_points: list[ControlPoint] = []
+index = 0
+moving_index = -1
 
 run = True
 while run:
@@ -21,9 +25,54 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
+
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            m_left, m_middle, m_right = pygame.mouse.get_pressed()
+            if m_left:
+                x, y = pygame.mouse.get_pos()
+                found = False
+                for cp in control_points:
+                    if cp.is_bellow(x, y):
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                        moving_index = control_points.index(cp)
+                        found = True
+                if not found:
+                    control_points.append(ControlPoint(
+                        x, y, index))
+                    index += 1
+            elif m_right:
+                x, y = pygame.mouse.get_pos()
+                for cp in control_points:
+                    if cp.is_bellow(x, y):
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                        control_points.remove(cp)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            moving_index = -1
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+        if moving_index != -1:
+            x, y = pygame.mouse.get_pos()
+            control_points[moving_index].set_pos(x, y)
+
+    # draws lines connecting control points
+    for i in range(len(control_points) - 1):
+        pygame.draw.line(
+            screen, black, control_points[i].get_pos(), control_points[i+1].get_pos(), width=2)
+    # draws control points
+    for p in control_points:
+        p.draw(screen, red)
+    # draws text for less than 4 cp
+    if len(control_points) < 4:
+        text = font.render(
+            f'You need at least 4 control points, {4 - len(control_points)} to go.', True, black)
+        textRect = text.get_rect()
+        textRect.centerx = screen.get_size()[0] // 2
+        screen.blit(text, textRect)
 
     pygame.display.update()
 
