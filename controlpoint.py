66b124e7306c2185fc101a8c43953b632bdf9621
Paulo Albuquerque <paulora2405@gmail.com>
2022-06-18 11:56:52 -0300

    Add Control Point color attribute

--- controlpoint.py
+++ controlpoint.py
@@ -1,17 +1,16 @@
 import pygame
 from colors import *
 
-pygame.init()
-font = pygame.font.Font('freesansbold.ttf', 16)
-
 
 class ControlPoint:
-    def __init__(self, x: int, y: int, id: int):
+    def __init__(self, x: int, y: int, id: int, color: tuple[int, int, int]):
         self.x = x
         self.y = y
         self.id = id
         self.inner_circle = None
         self.outter_circle = None
+        self.color = color
+        self.font = pygame.font.Font('freesansbold.ttf', 16)
 
     def get_pos(self) -> tuple[int, int]:
         return (self.x, self.y)
@@ -23,8 +22,8 @@ class ControlPoint:
     def is_bellow(self, x: int, y: int):
         return self.outter_circle.collidepoint(x, y)
 
-    def draw(self, screen: pygame.Surface, color):
-        text = font.render(f'P{self.id}', True, color)
+    def draw(self, screen: pygame.Surface):
+        text = self.font.render(f'P{self.id}', True, self.color)
         textRect = text.get_rect()
         if self.y < 20:
             textRect.center = (self.x, self.y + 20)
@@ -33,6 +32,6 @@ class ControlPoint:
 
         screen.blit(text, textRect)
         self.outter_circle = pygame.draw.circle(
-            screen, color, (self.x, self.y), 10)
+            screen, self.color, (self.x, self.y), 10)
         self.inner_circle = pygame.draw.circle(
             screen, grey, (self.x, self.y), 9)
